3a9cebcf4c7ef11527556d8b80c6978707b02cd2
saman esmailpour <saman.esmailpur@gmail.com>
2024-02-21 12:30:51 +0330

    first commit

---
+++ sam_store/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from sam_store.users.models import User
+from sam_store.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
